0f16c460452583da2ff32b014651c1bfc8ae85c9
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-08 16:47:46 -0500

    Create views and controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
-  end
+  # resources :users, only: [:index, :show] do
+  #   resources :posts, only: [:index, :show]
+  # end
   root to: "users#index"
+  get "/users/", to: "users#index"
+  get "/users/:id", to: "users#show"
+  get "/users/:id/posts/", to: "posts#index"
+  get "/users/:id/posts/:id", to: "posts#show"
 end
