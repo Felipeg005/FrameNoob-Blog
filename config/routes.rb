c3bf3db7a12d9fcd6bb600cfa48b39721f996fb1
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-08 17:23:21 -0500

    Install linters

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 Rails.application.routes.draw do
-  # resources :users, only: [:index, :show] do
-  #   resources :posts, only: [:index, :show]
-  # end
   root to: "users#index"
   get "/users/", to: "users#index"
   get "/users/:id", to: "users#show"
