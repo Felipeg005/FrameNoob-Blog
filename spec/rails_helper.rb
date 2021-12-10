8176d14d6a3a172f60b372437a52e2ef9c237c17
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-09 19:29:05 -0500

    Add spec files for users and post with multiple scenarios

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -31,6 +31,7 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  $VERBOSE = nil
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,21 +1,43 @@
-require './rails_helper'
+require 'rails_helper'
 
-RSpec.describe 'Users', :type => :request do
-  it "creates a Widget and redirects to the Widget's page" do
-    get '/users'
-    expect(response).to be_successful
-    expect(response.body).to include('Here is a list of all users')
-    # expect(response).to render_template(:index)
-
-    # expect(response).to redirect_to(assigns(:users))
-    # follow_redirect!
-
-    # expect(response).to render_template(:index)
-    # expect(response.body).to include('Here is a list of all users')
+RSpec.describe UsersController, :type => :request do
+  describe "Testing users index action, controller, routes and views" do
+    it "checks if response status was correct" do
+      get '/users'
+      expect(response).to be_successful
+      expect(response).to have_http_status(:ok)
+    end
+    it "checks if a correct template was rendered" do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+    it "checks if the response body includes correct placeholder text" do
+      get '/users'
+      expect(response.body).to include('Here is a list of all users')
+    end
+    it "does not render a different template" do
+      get ('/users')
+      expect(response).to_not render_template(:show)
+    end
   end
 
-  # it "does not render a different template" do
-  #   get ('/users')
-  #   expect(response).to_not render_template(:show)
-  # end
+  describe "Testing users show action, controller, routes and views" do
+    it "checks if response status was correct" do
+      get '/users/user_id'
+      expect(response).to be_successful
+      expect(response).to have_http_status(:ok)
+    end
+    it "checks if a correct template was rendered" do
+      get '/users/user_id'
+      expect(response).to render_template(:show)
+    end
+    it "checks if the response body includes correct placeholder text" do
+      get '/users/user_id'
+      expect(response.body).to include('Here is all user history information')
+    end
+    it "does not render a different template" do
+      get ('/users/user_id')
+      expect(response).to_not render_template(:index)
+    end
+  end
 end
