9bd3f56f8f295ff3cfa527931e3771c50ec62931
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-13 12:56:29 -0500

    Fix linters 1.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   def current_user
     # should return the first user from a database
-    get_current_user = User.first
-    get_current_user
+    User.first
   end
 end
