346824d57fa82aa16722ac63c70fb9fa173a48a3
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-13 12:26:06 -0500

    Add methods for likes and comments model

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,3 +1,8 @@
 class Like < ApplicationRecord
   belongs_to :user, :post
+
+  def update_likes_counter(id, data)
+    # A method that updates the comments counter for a post.
+    Post.find(id).update(comments_counter: data)
+    end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,9 +2,9 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, :likes, foreign_key: 'post_id'
 
-  def update_likes_counter(id, data)
+  def update_post_counter(id, data)
   # A method that updates the posts counter for a user.
-  self.find(id).update(likes_counter: data)
+  User.find(id).update(likes_counter: data)
   end
 
   def recent_comments
