7f9add62ec515107d8ada146fa194b2898991ca7
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-16 11:00:46 -0500

    Apply required changes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
+    @user = User.find(params[:id])
   end
 
   def show
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-user1 = User.create(id: 1, name: "Rudolph", photo: "link#", bio: "I love dancing", posts_counter: 1)
-user2 = User.create(id: 2, name: "Mike", photo: "link#", bio: "I love hiking", posts_counter: 3)
+user1 = User.create(id: 1, name: "Rudolph", photo: "link#", bio: "I love dancing", posts_counter: 2)
+user2 = User.create(id: 2, name: "Mike", photo: "link#", bio: "I love hiking", posts_counter: 0)
 p "Created #{User.count} Users"
 
 post1 = Post.create!(id: 1, author_id: user1.id, title: "my first post", text: "dancing its great", comments_counter: 1, likes_counter: 2)
