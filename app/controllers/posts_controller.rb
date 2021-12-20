1b706c75d4ea2b997a0103a1bc95d22f1afe6950
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-20 16:56:34 -0500

    finish form for current_user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,14 +18,41 @@ class PostsController < ApplicationController
 
   def create
     @user = current_user
-    @post = Post.new(post_params)
+    @post = Post.create!(post_params)
     respond_to do |format|
       format.html do
           if @post.save
               # success message
               flash[:success] = "Post saved successfully"
               # redirect to index
-              redirect_to "/users/#{@user.id}/"
+              redirect_to "/users/#{@post.author_id}/posts"
+          else
+              # error message
+              flash.now[:error] = "Error:  Post could not be saved"
+              # render new
+              render :new, locals: { post: @post }
+          end
+      end
+    end
+  end
+
+  def new_comment
+    @comment = Comment.new
+    respond_to do |format|
+      format.html { render :new_comment }
+    end
+  end
+
+  def create_commment
+    @user = current_user
+    @comment = Comment.create!(comment_params)
+    respond_to do |format|
+      format.html do
+          if @post.save
+              # success message
+              flash[:success] = "Post saved successfully"
+              # redirect to index
+              redirect_to "/users/#{@post.author_id}/posts"
           else
               # error message
               flash.now[:error] = "Error:  Post could not be saved"
@@ -38,6 +65,12 @@ class PostsController < ApplicationController
 
   private
   def post_params
-    params.require(:post).permit(:title, :text)
+    params.require(:post).permit(:author_id, :title, :text, :comments_counter, :likes_counter)
   end
+  
+  def post_params
+    params.require(:post).permit(:author_id, :title, :text, :comments_counter, :likes_counter)
+  end
+
+
 end
