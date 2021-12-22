17da37cbbae87df2a8546edc8b34c3b8b98f3c68
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-22 14:50:38 -0500

    Fix linters 1

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -12,23 +12,24 @@ class CommentsController < ApplicationController
     respond_to do |format|
       format.html do
         if @comment.save
-            # success message
-            flash[:success] = "Post saved successfully"
-            # redirect to index
-            redirect_to "/users/#{@comment.author_id}/posts/#{@comment.post_id}"
+          # success message
+          flash[:success] = 'Post saved successfully'
+          # redirect to index
+          redirect_to '/users/#{@comment.author_id}/posts/#{@comment.post_id}'
         else
-            # error message
-            flash.now[:error] = "Error:  Post could not be saved"
-            # render new
-            render :new, locals: { post: @comment }
+          # error message
+          flash.now[:error] = 'Error:  Post could not be saved'
+          # render new
+          render :new, locals: { post: @comment }
         end
       end
     end
     Post.update_comments_counter(@comment.post_id)
   end
-    private
 
-    def comment_params
-      params.require(:comment).permit(:author_id, :post_id, :text)
-    end
+  private
+
+  def comment_params
+    params.require(:comment).permit(:author_id, :post_id, :text)
+  end
 end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -9,9 +9,10 @@ class LikesController < ApplicationController
     Post.update_likes_counter(@like.post_id)
     redirect_to "/users/#{@like.author_id}/posts/#{@like.post_id}"
   end
-    private
 
-    def like_params
-      params.require(:like).permit(:author_id, :post_id)
-    end
+  private
+
+  def like_params
+    params.require(:like).permit(:author_id, :post_id)
+  end
 end
