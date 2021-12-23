efd124714b8e589292fbd2791b256aec25cd127b
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-22 19:28:12 -0500

    Add validation for post title, comments and likes counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,10 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
+  validates :title, presence: true
+  validates :title, length: { maximum: 250 }
+  validates :comments_counter, numericality: { only_integer: true, :greater_than_or_equal_to 0 }
+  validates :likes_counter, numericality: { only_integer: true, :greater_than_or_equal_to 0 }
 
   def recent_comments
     # A method which returns the 5 most recent comments for a given post.
