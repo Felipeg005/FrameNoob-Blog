eb4d52ae19ba25eb6a8ca56b8ff7411c2a55f869
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-12-23 14:02:39 -0500

    Fix linters 1.1

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -63,7 +63,7 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-  
+
   config.after(:suite) do
     DatabaseCleaner.strategy = :transaction
     DatabaseCleaner.clean_with(:truncation)
