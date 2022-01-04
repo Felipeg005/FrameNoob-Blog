6b5385645a91de9c7818d3c9f783b80d28a60888
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-04 12:45:01 -0500

    Reset password activated and final configuration for email confirmation

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,20 +36,20 @@ Rails.application.configure do
   config.action_mailer.perform_caching = false
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-      address:              'smtp.gmail.com',
-      port:                  465,
-      domain:               'gmail.com',
-      user_name:            'your email',
-      password:             'your password',
-      authentication:       'plain',
-      enable_starttls_auto: true,
-      ssl: true
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #     address:              'smtp.gmail.com',
+  #     port:                  465,
+  #     domain:               'gmail.com',
+  #     user_name:            'test@gmail.com',
+  #     password:             'somepassword',
+  #     authentication:       'plain',
+  #     enable_starttls_auto: true,
+  #     ssl: true
 
-  }
+  # }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
