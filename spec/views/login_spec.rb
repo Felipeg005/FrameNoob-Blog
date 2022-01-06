6d4cbe1cf33d10f6752fe3ffc6ed5235eeef5a08
Felipeg005 <juanfelipegomeztobon@gmail.com>
2022-01-06 13:12:09 -0500

    Add tests for login view

--- spec/views/login_spec.rb
+++ spec/views/login_spec.rb
@@ -1,11 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :view do
-  subject { Comment.new(author_id: 1, post_id: 1, text: 'Lorem Ipsum is simply dummy text of the printing') }
-  describe 'Testing comment model to behave as expected' do
-    it 'checks if the response body includes correct placeholder text' do
-      get '/users/1/posts'
-      expect(response.body).to include('Here is a list of posts all posts for the user')
+RSpec.describe PostsController, type: :feature do
+  describe 'Testing Login index action, controller, routes and view' do
+    # before(:each) do
+    #   visit '/users/sign_in'
+    #   within("#new_user") do
+    #     fill_in 'user_email', with: '1@gmail.com'
+    #     fill_in 'user_password', with: '1234567'
+    #   end
+    #   click_button 'Log in'
+    # end
+
+    it 'checks if page status was correct' do
+      visit '/users/sign_in'
+      expect(page).to have_http_status(:ok)
     end
+
+    it 'checks if page renders username and password inputs and the "Submit" button' do
+      visit '/users/sign_in'
+      expect(page).to have_selector('#user_email')
+      expect(page).to have_selector('#user_password')
+      expect(page).to have_selector('.actions')
+      expect(page).to have_content('Log in')
+    end
+
+    it 'checks if click the submit button without filling in the username and the password, I get a detailed error' do
+      visit '/users/sign_in'
+      within("#new_user") do
+        fill_in 'user_email', with: ''
+        fill_in 'user_password', with: ''
+      end
+      click_button 'Log in'
+      expect(page).to have_content('Invalid Email or password')
+    end
+
+    it 'checks if click the submit button after filling in the username and the password with incorrect data, I get a detailed error' do
+      visit '/users/sign_in'
+      within("#new_user") do
+        fill_in 'user_email', with: 'Ruperth'
+      end
+      click_button 'Log in'
+      expect(page).to have_content('Invalid Email or password')
+    end
+
+    it 'checks if click the submit button after filling in the username and the password with correct data, I am redirected to the root page' do
+      visit '/users/sign_in'
+      within("#new_user") do
+        fill_in 'user_email', with: '1@gmail.com'
+        fill_in 'user_password', with: '1234567'
+      end
+      click_button 'Log in'
+      expect(page).to have_current_path('/')
+    end
+
   end
 end
+
